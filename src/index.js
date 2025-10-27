f7699f9558afd275799930f19b8ab6036c40d6cb
Daga1984 <jesusdavidgarces84@gmail.com>
2025-10-27 14:40:43 -0500

    back

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import mongoose from "mongoose";
 import app from "./app.js";
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://garcessalasdavid_db_user:bK42MHlNzh42BFpo@cluster0.74inyvo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+import mongoose from "mongoose";
 
-    console.log("✅ Conectado a MongoDB Atlas (modo DEV)");
-  } catch (error) {
-    console.error("❌ Error al conectar con MongoDB Atlas:", error.message);
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+  try {
+    await mongoose.connect("mongodb+srv://garcessalasdavid_db_user:bK42MHlNzh42BFpo@cluster0.74inyvo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("✅ Conectado a MongoDB Atlas");
+  } catch (err) {
+    console.error("❌ Error al conectar con MongoDB:", err.message);
   }
 };
 
